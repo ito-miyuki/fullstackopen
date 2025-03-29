31e6e8cede1b06fe378ddaaccfda67794272e180
ito-miyuki <mitou.1221@gmail.com>
2025-03-29 12:17:56 +0200

    added part1

---
+++ part1/src/App.jsx
@@ -0,0 +1,46 @@
+const Part = (props) => {
+  return (
+    <p>{props.parts.part} {props.parts.exercise}</p>
+  )
+}
+
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>  
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part parts={props.parts[0]} />
+      <Part parts={props.parts[1]} />
+      <Part parts={props.parts[2]} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.parts[0].exercise + props.parts[1].exercise + props.parts[2].exercise}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    {part: 'Fundamentals of React', exercise: 10},
+    {part: 'Using props to pass data', exercise: 2},
+    {part: 'State of a component', exercise: 14}
+  ]
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  )
+}
+
+export default App
