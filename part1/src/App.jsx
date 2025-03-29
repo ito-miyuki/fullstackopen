23987897dccdec2a04966dbded36333131c6b4cd
ito-miyuki <mitou.1221@gmail.com>
2025-03-29 15:50:03 +0200

    replaced with object

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,14 +1,10 @@
 /* eslint-disable react/prop-types */
 
-const Part = (props) => {
-  return (
-    <p>{props.parts.part} {props.parts.exercise}</p>
-  )
-}
-
 const Header = (props) => {
+  console.log(typeof(props));
+  console.log(props.course);
   return (
-    <h1>{props.course}</h1>  
+    <h1>{props.course.name}</h1>  
   )
 }
 
@@ -28,19 +24,43 @@ const Total = (props) => {
   )
 }
 
+const Part = (props) => {
+  return (
+    <p>{props.parts.name} {props.parts.exercise}</p>
+  )
+}
+
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {part: 'Fundamentals of React', exercise: 10},
-    {part: 'Using props to pass data', exercise: 2},
-    {part: 'State of a component', exercise: 14}
-  ]
+  // const course = 'Half Stack application development'
+  // const parts = [
+  //   {part: 'Fundamentals of React', exercise: 10},
+  //   {part: 'Using props to pass data', exercise: 2},
+  //   {part: 'State of a component', exercise: 14}
+  // ]
+
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercise: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercise: 2
+      },
+      {
+        name: 'State of a component',
+        exercise: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
