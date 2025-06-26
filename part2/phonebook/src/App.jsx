173862baade3e4937b9133584cd054551b98abba
ito-miyuki <mitou.1221@gmail.com>
2025-06-26 15:46:59 +0300

    phonebook functioning

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,24 +1,58 @@
 import { useState } from 'react'
 
 const App = () => {
-  const App = () => {
-    const [persons, setPersons] = useState([
-      { name: 'Arto Hellas' }
-    ]);
-    const [newName, setNewName] = useState('');
+
+  // this is dummy data
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
+  // const [persons, setPersons] = useState([]);
+  
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const isDuplicated = persons.some(person => person.name === newName);
+
+    if (isDuplicated) {
+      alert(`${newName} is already added to phonebook`);
+      return ;
+    }
+
+    console.log('new name added! [newName]: ', newName);
+
+    const newPerson = { name: newName, number: newNumber };
+    setPersons(persons.concat(newPerson));
+    setNewName('');
   }
+
+    const filteredPersons = persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()));
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <div>filter shown with <input value={filter} onChange={(e) => setFilter(e.target.value)}/></div>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+        {filteredPersons.map((person, index) => {
+          return <div key={index}>{person.name}: {person.number}</div>
+        })}
     </div>
   )
 }
